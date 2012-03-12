04979315bd986114363ed1b11e7dcc9cc971dde6
xi <chenxifr@gmail.com>
2012-03-12 01:04:39 +0100

    Added wp theme

---
+++ Serenity/wp-themes/Serenity-G/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="fragment" content="!">
+    <meta name="url" content="<?php bloginfo('url'); ?>">
+    <?php include (TEMPLATEPATH . "/site-verify-code.php"); ?>
+    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+    <title>Serenity | Xi CHEN's blog</title>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/serenity/gwt.4.wp.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/serenity/serenity.nocache.js"></script>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+  	<iframe src="javascript:''" id="__gwt_historyFrame" tabIndex='-1' style="position:absolute;width:0;height:0;border:0"></iframe>
+    <noscript>
+      <div style="width: 22em; position: absolute; left: 50%; margin-left: -11em; color: red; background-color: white; border: 1px solid red; padding: 4px; font-family: sans-serif">
+        Your web browser must have JavaScript enabled
+        in order for this application to display correctly.
+      </div>
+    </noscript>
+  </body>
+</html>
