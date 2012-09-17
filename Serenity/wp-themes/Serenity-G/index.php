998784122d01ad1003361f585e8bbd2fef130dfd
xi <chenxifr@gmail.com>
2012-09-17 22:01:42 +0200

    separate header body and footer

--- Serenity/wp-themes/Serenity-G/index.php
+++ Serenity/wp-themes/Serenity-G/index.php
@@ -1,24 +1,10 @@
-<!doctype html>
-<html>
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="fragment" content="!">
-    <meta name="url" content="<?php bloginfo('url'); ?>">
-    <?php include (TEMPLATEPATH . "/site-verify-code.php"); ?>
-    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
-    <title>Serenity | Xi CHEN's blog</title>
-    <!-- before your module(*.nocache.js) loading  -->
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    <!--[if IE 7]>
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/serenity/css/font-awesome-ie7.css">
-    <![endif]-->
-    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/serenity/gwt.4.wp.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/serenity/serenity.nocache.js"></script>
-    <?php wp_head(); ?>
-  </head>
-  <body>
+<?php
+/**
+ * @package seanchenxi
+ * @subpackage Serenity-G
+ * @since Serenity-G 1.0
+ */
+get_header(); ?>
   	<iframe src="javascript:''" id="__gwt_historyFrame" tabIndex='-1' style="position:absolute;width:0;height:0;border:0"></iframe>
     <noscript>
       <div style="width: 22em; position: absolute; left: 50%; margin-left: -11em; color: red; background-color: white; border: 1px solid red; padding: 4px; font-family: sans-serif">
@@ -26,5 +12,4 @@
         in order for this application to display correctly.
       </div>
     </noscript>
-  </body>
-</html>
+<?php get_footer(); ?>
