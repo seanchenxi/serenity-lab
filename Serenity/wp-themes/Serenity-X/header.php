e61df9fa3ac3ec41beb7cf60f9fdb6df88c57e1d
Xi CHEN <chenxifr@gmail.com>
2013-02-21 21:22:12 +0100

    simplify html + css

--- Serenity/wp-themes/Serenity-X/header.php
+++ Serenity/wp-themes/Serenity-X/header.php
@@ -5,10 +5,10 @@
     <meta name="fragment" content="!">
     <meta name="url" content="<?php bloginfo('url'); ?>">
     <?php include (TEMPLATEPATH . "/site-verify-code.php"); ?>
-    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
     <title>Serenity | Xi CHEN's blog</title>
     <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/serenity/gwt.4.wp.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/serenity/serenity.nocache.js"></script>
+    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
     <?php wp_head(); ?>
   </head>
   <body>
